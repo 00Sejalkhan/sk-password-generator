d9777393b74c8be468b51eeb027c865df2dbf08e
_SEJALKHAN_ <164911951+00Sejalkhan@users.noreply.github.com>
2025-04-23 14:53:47 +0500

    Add files via upload

---
+++ password generator/project.py
@@ -0,0 +1,28 @@
+#project 6: password Generator In python
+#Instructor: Sejal khan
+
+
+import random
+import string      
+
+def generated_password(length=12):
+     characters = string.ascii_letters + string.digits + string.punctuation
+     password = ''.join(random.choice (characters) for _ in range(length))
+     return password  
+
+#users inputs
+
+length = int(input("Enter the length of your desired password: "))
+
+password = generated_password (length)
+
+print("Your Desired Generated Password:" , password)
+
+
+
+
+
+
+
+
+
